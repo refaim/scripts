d113b6d78134e33fad939f6b7242199f0c4c655d
Roman Kharitonov <refaim.vl@gmail.com>
2011-04-17 14:05:47 +1100

    booksync: now use memory card

--- booksync.bat
+++ booksync.bat
@@ -7,30 +7,8 @@ if [%1] == [] (
     set disk=%1\
 )
 set src="G:\files\library\fiction\books-good"
-set dst=temp
+set dst=%disk%
 
-goto main
-
-:silent
-    %* >nul 2>&1
-    exit /b 0
-
-:main
-pushd %disk%
 if errorlevel 1 exit 1
-
-echo Prepare for copying...
-call :silent mkdir %dst%
-for /F "delims=" %%f in ('dir /B "*, *"') do call :silent move /Y "%%f" %dst%
-call :silent move /Y unread %dst%
-
 echo Copying...
 robocopy %src% %dst% /MIR
-
-echo Restoring working state...
-pushd %dst%
-for /F "delims=" %%f in ('dir /B "*.*"') do call :silent move /Y "%%f" %disk%
-popd
-rmdir %dst%
-
-popd
